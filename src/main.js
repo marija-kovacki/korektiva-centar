ae1231fd1fbc15aac606d67a29506bd575546c2b
marija kovacki <kurucevm@gmail.com>
2025-06-16 18:08:32 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,138 @@
+import { reviews } from './reviews.js'
+
+//Mobile sidebar
+const hamburger = document.getElementById('hamburgerToggle')
+const sidebar = document.getElementById('mobileSidebar')
+const overlay = document.getElementById('overlay')
+const sidebarLinks = sidebar.querySelectorAll('a')
+
+function openSidebar() {
+  sidebar.classList.add('open')
+  overlay.classList.add('show')
+  document.body.classList.add('noscroll')
+}
+
+function closeSidebar() {
+  sidebar.classList.remove('open')
+  overlay.classList.remove('show')
+  document.body.classList.remove('noscroll')
+}
+
+hamburger.addEventListener('click', openSidebar)
+overlay.addEventListener('click', closeSidebar)
+
+sidebarLinks.forEach((link) => {
+  link.addEventListener('click', closeSidebar)
+})
+
+//Gallery animation
+function animateCount(el) {
+  const target = +el.dataset.target
+  const duration = 1500
+  const increment = target / (duration / 30)
+
+  let current = 0
+
+  const update = () => {
+    current += increment
+    if (current < target) {
+      el.textContent = Math.floor(current)
+      requestAnimationFrame(update)
+    } else {
+      el.textContent = target
+    }
+  }
+
+  update()
+}
+
+//Gallery carousel
+$(document).ready(function () {
+  $('#gallery').owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+  })
+})
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const counters = entry.target.querySelectorAll('.count')
+        counters.forEach((counter) => animateCount(counter))
+        observer.unobserve(entry.target)
+      }
+    })
+  },
+  {
+    threshold: 0.5,
+  }
+)
+
+const statsSection = document.getElementById('stats')
+observer.observe(statsSection)
+
+const reviewsContainer = document.getElementById('reviews-carousel')
+
+//Reviews elements
+reviews.forEach(({ name, rating, comment }) => {
+  const reviewEl = document.createElement('div')
+  reviewEl.classList.add('review-card')
+
+  reviewEl.innerHTML = `
+    <div class="review">
+      <h5>${name}</h5>
+      <p>${'⭐'.repeat(rating)}</p>
+      <p class='comment'>${comment}</p>
+    </div>
+  `
+
+  reviewsContainer.appendChild(reviewEl)
+})
+
+//Reviews carousel
+$(document).ready(function () {
+  $('#reviews-carousel').owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1024: {
+        items: 3,
+      },
+    },
+  })
+})
+
+//FAQ accordion
+const faqItems = document.querySelectorAll('.faq-item')
+
+faqItems.forEach((item) => {
+  const question = item.querySelector('.faq-question')
+
+  question.addEventListener('click', () => {
+    // Zatvori sve ostale
+    faqItems.forEach((i) => {
+      if (i !== item) {
+        i.classList.remove('active')
+      }
+    })
+
+    // Toggle trenutno
+    item.classList.toggle('active')
+  })
+})
---
+++ src/reviews.js
@@ -0,0 +1,108 @@
+export const reviews = [
+  {
+    name: "Nemanja B.",
+    rating: 5,
+    comment:
+      "Osoblje je veoma sposobno i profesionalno, poseduju izuzetno znanje u svojoj oblasti. Oni sa vama prolaze kroz sve vežbe, terapije i slično sa velikim strpljenjem i ne komplikuju objašnjenja",
+  },
+  {
+    name: "Boban M.",
+    rating: 5,
+    comment: "Veoma ljubazno, korisno, strpljivo, lepo iskustvo.",
+  },
+  {
+    name: "Milan D.",
+    rating: 5,
+    comment: "Sjajni ljudi i odlična usluga :D",
+  },
+  {
+    name: "Emina Š.",
+    rating: 5,
+    comment: "Profesionalno, odlična usluga.",
+  },
+  {
+    name: "Ivana R.",
+    rating: 5,
+    comment: "Sve pozitivno 😍 ❤❤❤",
+  },
+  {
+    name: "Anica S.",
+    rating: 5,
+    comment: "Kao kod kuće!",
+  },
+  {
+    name: "Tijana S.",
+    rating: 5,
+    comment: "Sve preporuke!!!!",
+  },
+  {
+    name: "Milan M.",
+    rating: 5,
+    comment: "",
+  },
+    {
+    name: "Nada M.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Etel V.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Dubravka P.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Brankica M.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Fjodor C.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Opel Circom Zrenjanin",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Dragana G.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Saska C.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Dragana K.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Vera F.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Goran J.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Milica K.",
+    rating: 5,
+    comment: "",
+  },
+      {
+    name: "Ratko N.",
+    rating: 5,
+    comment: "",
+  },
+];
